d9dddfe4b6b053d6b1b81d5fca9681418d5c3fc2
D3zD3adWinn3r <Desireenlopez5@gmail.com>
2025-05-01 20:42:25 -0500

    index

---
+++ index.js
@@ -0,0 +1,23 @@
+function    convertToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+function describeTemperature(fahrenheit) {
+    const celsius = convertToCelsius(fahrenheit);
+    if (celsius < 0) {
+        return "very cold!";
+    } else if (celsius < 20) {
+        return "cold!";
+    } else if (celsius < 30) {
+        return "warm!";
+    } else if (celsius < 40) {
+        return "hot!";
+    } else if (celsius >= 40){   
+        return "very hot!";
+    }
+}
+function convertAndDescribe(fahrenheit) {
+    const celsius = convertToCelsius(fahrenheit);
+    const description = describeTemperature(fahrenheit);
+         return `The temperature is ${celsius.toFixed(2)}°C, which is considered ${description}`;
+}
+console.log(convertAndDescribe(32)); // The temperature is 0.00°C, which is considered cold!
